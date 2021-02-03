47194b8b9318710995c47da4b5a0835599f57da8
Ritvik Gupta <guptaritvik42@gmail.com>
2021-02-03 22:16:28 +0530

    Challenge_03

---
+++ Challenge_03.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int amt,bank_balance;
+    cout<<"enter the bank balance";
+    cin>>bank_balance;
+    cout<<"enter the amount to be widthdrawn from the bank";
+    if(amt%5==0){
+        cin>>amt;
+    }
+    return 0;
+}
