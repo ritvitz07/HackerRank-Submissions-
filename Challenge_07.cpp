67e3271bdc1a218447e638cadafac93b2ca6d45e
Ritvik Gupta <guptaritvik42@gmail.com>
2021-02-05 00:06:31 +0530

    Challenge_07

--- Challenge_07.cpp
+++ Challenge_07.cpp
@@ -5,17 +5,10 @@ int main(){
 
     int a,b,T,temp;
     cin>>T;
-  /*  for(int i=0;i<T;i++){
-        cin>>a>>b;
-        temp=a%b;
-
-    }*/
     while(T--){
         cin>>a>>b;
         temp=a%b;
-        cout<<temp;
+        cout<<temp<<endl;
     }
-    
-
     return 0;
 }
